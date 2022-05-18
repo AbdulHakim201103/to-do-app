4069793416de0eead2f95009bf49061a3d6197c5
AbdulHakim201103 <abdulhakim201103@gmail.com>
2022-05-18 23:16:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./Pages/Home/Home/Home";
+import Navbar from "./Pages/Home/Navbar/Navbar";
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
+import NotFound from "./Pages/NotFound/NotFound";
+function App() {
+  return (
+    <div className="container mx-auto">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
